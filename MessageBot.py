4849988ffd30e2aea399e88d16ab432dfc99dfa2
vtoropov <vtoropov@quantumbrains.com>
2021-04-16 14:14:13 +0300

    Initial commit

---
+++ MessageBot.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import telebot
+import json
+
+PROOFIT_BOT = "Proofit_test_bot"
+WHITELIST_BOT = "Whitelist_errors_bot"
+
+with open('Tokens.json') as json_file:
+    tokens = json.loads(json_file.read())
+bot = telebot.TeleBot(tokens[PROOFIT_BOT])
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "/help":
+        bot.send_message(message.from_user.id, "All right!")
+    elif message.text == "go":
+        # id = bot.get_chat(message.from_user.id).id
+        id = message.from_user.i
+        bot.send_message(message.from_user.id, "message_id: {}".format(id))
+    else:
+        bot.send_message(message.from_user.id, "I don't understand! Please write /help.")
+
+
+bot.polling(none_stop=True, interval=0)
